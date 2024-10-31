6d65531a1a5a16dbe906f1d1294738d4ac759b39
Delzarkafashi <145854766+Delzarkafashi@users.noreply.github.com>
2024-10-31 08:29:35 +0100

    fixat med fuktioner till log/sing in och lite andra css

--- FrontEnd/src/components/Footer/Footer.jsx
+++ FrontEnd/src/components/Footer/Footer.jsx
@@ -4,9 +4,19 @@ import './Footer.scss';
 const Footer = () => {
   return (
     <div className="Footer">
-      <div className="div1">Innehåll 1</div>
-      <div className="div2">Innehåll 2</div>
-      <div className="div3">Innehåll 3</div>
+      <div className="div1">
+        <h4>Om Oss</h4>
+        <p>Information om företaget eller sidan.</p>
+      </div>
+      <div className="div2">
+        <h4>Kontakt</h4>
+        <p>Email: info@exempel.se</p>
+        <p>Telefon: 123-456789</p>
+      </div>
+      <div className="div3">
+        <h4>Följ Oss</h4>
+        <p>Sociala medier länkar eller ikoner.</p>
+      </div>
     </div>
   );
 };
