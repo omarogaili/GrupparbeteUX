5ea12aa25fbe5c8aa71fe56db99c053e6882bd91
Delzarkafashi <145854766+Delzarkafashi@users.noreply.github.com>
2024-11-01 10:43:32 +0100

    fronten

---
+++ FrontEnd/src/Pages/ViewItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header/Header';
+import Body from '../components/Body/Body';
+import Footer from '../components/Footer/Footer';
+
+const ViewItem = () => {
+  return (
+    <div>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+};
+
+export default ViewItem;
--- FrontEnd/src/components/Header/Header.jsx
+++ FrontEnd/src/components/Header/Header.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
-import './Header.scss'; 
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Header.scss';
+import AuthModal from '../LogSingIn/AuthModal';
 
 const Header = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
   return (
     <div className="Header">
-      <div className="LogaNamn">Logga/Namn</div>
-      <div className="Kategorier">Kategorier</div>
-      <div className="LagInAnnons">Lägg in annons</div>
-      <div className="Search">Sök</div>
-      <div className="LogSignIn">Logga in / Registrera</div>
+      <Link to="/" className="LogaNamn">Logga/Namn</Link>
+      <Link to="/view-item" className="Kategorier">Kategorier</Link>
+      <Link to="/mina-annonser/lagg-in-annons" className="LagInAnnons">Lägg in annons</Link>
+      <Link to="/annonser" className="Search">Annonser</Link>
+      <span onClick={openModal} className="LogSignIn">Logga in / Registrera</span>
+
+      {isModalOpen && <AuthModal onClose={closeModal} />}
     </div>
   );
 };
