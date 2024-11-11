83fc40342a2c5314eb460a0983393c84e00281d7
Ahmedm100 <ahmmah1220@gmail.com>
2024-11-11 11:28:01 +0100

    Changed design to be more like Figma

--- FrontEnd/src/components/Header/Header.jsx
+++ FrontEnd/src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 import './Header.scss';
 import AuthModal from '../LogSingIn/AuthModal';
 import CategoryDropdown from '../CategoryDropdown/CategoryDropdown';
@@ -17,11 +19,15 @@ const Header = () => {
         <CategoryDropdown /> {/* Placera dropdown här */}
       </div>
       <Link to="/mina-annonser/lagg-in-annons" className="LagInAnnons">Lägg in annons</Link>
-      <Link to="/annonser" className="Search">Annonser</Link>
-      <span onClick={openModal} className="LogSignIn">Logga in / Registrera</span>
+      
+      <span onClick={openModal} className="LogSignIn">
+        <FontAwesomeIcon icon={faUser} size="lg" /> {/* Login Icon */}
+      </span>
       {isModalOpen && <AuthModal onClose={closeModal} />}
     </div>
   );
 };
 
 export default Header;
+
+
